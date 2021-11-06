04ece60ab0eae2bee65003ae7357452a1bd93aaa
satyajeetpandey <92566738+satyajeetpandey@users.noreply.github.com>
2021-11-06 13:04:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let question=document.getElementsByClassName("FAQ__title");
+
+for(let i=0;i<question.length;i++){
+    question[i].addEventListener("click",function(){
+        if(this.childNodes[1].classList.contains("fa-plus")){
+            this.childNodes[1].classList.remove("fa-plus");
+            this.childNodes[1].classList.add("fa-times");
+        }else{
+            this.childNodes[1].classList.remove("fa-times");
+            this.childNodes[1].classList.add("fa-plus");
+        }
+
+        let content =this.nextElementSibling;
+        if(content.style.maxHeight){
+            content.style.maxHeight=null;
+        }else{
+            content.style.maxHeight=content.scrollHeight + "px";
+        }
+        
+    });
+}
